56601e3c88c057196ab2558382d9d1adf2a86b63
angelfcor <angel.fcordova@gmail.com>
2026-07-27 19:49:46 -0600

    Ya da bien

--- hotelFortuna/vite.config.js
+++ hotelFortuna/vite.config.js
@@ -5,7 +5,7 @@ import * as glob from "glob";
 import HtmlCssPurgePlugin from "vite-plugin-purgecss";
 import HandlebarPlugin from "vite-plugin-handlebars";
 
-import getPageContext from "./src/data/";
+import getPageContext from "./src/data/index";
 
 function obtenerHtmlFiles() {
     return Object.fromEntries(
