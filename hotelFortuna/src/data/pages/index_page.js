b66138a19169bf1914a1b41095c762c248446fb7
angelfcor <angel.fcordova@gmail.com>
2026-07-27 20:20:34 -0600

    Header y Hero Avanzados

---
+++ hotelFortuna/src/data/pages/index_page.js
@@ -0,0 +1,41 @@
+export default {
+    tarifas: [
+        {
+            card_title: "2 Horas",
+            card_description: "Ideal para una estadía corta y rápida en el centro de Tegucigalpa.",
+            card_img: "https://picsum.photos/id/164/400/300",
+            card_label: "Corta estadía",
+            card_price: "250.00",
+            card_cta: "Reservar",
+        },
+        {
+            card_title: "5 Horas",
+            card_description: "Perfecta para descansar y recargar energías durante el día.",
+            card_img: "https://picsum.photos/id/165/400/300",
+            card_label: "Media estadía",
+            card_price: "550.00",
+            card_cta: "Reservar",
+        },
+        {
+            card_title: "5:00 p.m. a 9:00 a.m.",
+            card_description: "Disfruta una noche completa con todas las comodidades del hotel.",
+            card_img: "https://picsum.photos/id/166/400/300",
+            card_label: "Noche completa",
+            card_price: "1,100.00",
+            card_cta: "Reservar",
+        },
+        {
+            card_title: "2 Días",
+            card_description: "La mejor opción para una escapada en el centro.",
+            card_img: "https://picsum.photos/id/167/400/300",
+            card_label: "Fin de semana",
+            card_price: "2,200.00",
+            card_cta: "Reservar",
+        },
+    ],
+    contacto: {
+        direccion: "Costado Norte de la Iglesia Los Dolores, Centro de Tegucigalpa",
+        telefono1: "9733-5556",
+        telefono2: "8973-7935",
+    }
+}
