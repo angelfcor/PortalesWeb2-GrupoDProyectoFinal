3728908492a2d33eb058aee4b74d8a730bcdd2b9
Daniel Lobo <dani3ll0b017@gmail.com>
2026-07-31 13:03:45 -0600

    Agrega promociones, reservaciones y partials

---
+++ hotelFortuna/src/data/pages/promociones_page.js
@@ -0,0 +1,43 @@
+export default {
+    banner_title: "Promociones Exclusivas",
+    banner_subtitle: "Disfruta ofertas especiales para una estadía inolvidable.",
+    banner_img: "/img/sign.jpeg",
+
+    promociones: [
+        {
+            title: "Suite Deluxe",
+            description: "25% de descuento por 3 noches o más.",
+            img: "/img/room.jpeg",
+            alt: "Suite Deluxe",
+            label: "25% OFF"
+        },
+        {
+            title: "Escapada Romántica",
+            description: "Decoración especial y cena incluida.",
+            img: "/img/aisle-plants.jpeg",
+            alt: "Escapada Romántica",
+            label: "Especial"
+        },
+        {
+            title: "Todo Incluido",
+            description: "Alimentos, bebidas y actividades durante toda tu estadía.",
+            img: "/img/balcony.jpeg",
+            alt: "Todo Incluido",
+            label: "Todo Incluido"
+        },
+        {
+            title: "Plan Familiar",
+            description: "Niños menores de 10 años se hospedan gratis.",
+            img: "/img/outside.jpeg",
+            alt: "Plan Familiar",
+            label: "Familiar"
+        },
+        {
+            title: "Luna de Miel",
+            description: "Habitación premium, desayuno y detalles románticos.",
+            img: "/img/bed.jpeg",
+            alt: "Luna de Miel",
+            label: "Romántica"
+        }
+    ]
+};
---
+++ hotelFortuna/src/js/main.js
@@ -0,0 +1,7 @@
+import '../less/style.less';
+
+import { initPromociones } from './components/promociones.js';
+import { initReservaciones } from './components/reservaciones.js';
+
+initPromociones();
+initReservaciones();
