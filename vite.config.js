7673db1b23f1803c0b87689a4a913e94ea677c94
angelfcor <angel.fcordova@gmail.com>
2026-07-30 20:50:50 -0600

    fix: links ya no fallan

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig(
         appType: 'mpa',
         base: process.env.DEPLOY_BASE_URL ?? '/',
         build: {
-            rolldownOptions: {
+            rollupOptions: {
                 input: obtenerHtmlFiles()
             }
         },
