1d685d906495e654da28b91d637dc0ca4af0a553
angelfcor <angel.fcordova@gmail.com>
2026-08-02 12:09:06 -0600

    Corregidos algunos detalles

--- src/data/index.js
+++ src/data/index.js
@@ -1,15 +1,15 @@
-import indexContext from './pages/index_page';
-import nosotrosContext from './pages/nosotros_page';
-import contactoContext from './pages/contacto_page';
-import serviciosContext from './pages/servicios_page';
-import faqContext from './pages/faq_page';
-/*
-import habitacionesContext from './pages/habitaciones_page';
-/*import serviciosContext from './pages/servicios_page';*/
-import testimoniosContext from './pages/testimonios_page';
-/*import reservacionesContext from './pages/reservaciones_page';*/
+import indexContext from './pages/index_page.js';
+import nosotrosContext from './pages/nosotros_page.js';
+import contactoContext from './pages/contacto_page.js';
+import habitacionesContext from './pages/habitaciones_page.js';
+import serviciosContext from './pages/servicios_page.js';
+import faqContext from './pages/faq_page.js';
+import testimoniosContext from './pages/testimonios_page.js';
+import promocionesContext from './pages/promociones_page.js';
+import reservacionesContext from './pages/reservaciones_page.js';
 
 export default (page) => {
+    console.log(`Cargando contexto de: ${page}`);
     switch (page) {
         case '/index.html':
             return indexContext;
@@ -19,23 +19,18 @@ export default (page) => {
             return contactoContext;
         case '/habitaciones.html':
             return habitacionesContext;
-        /*
         case '/servicios.html':
             return serviciosContext;
         case '/faq.html':
             return faqContext;
-        /*
-    case '/habitaciones.html':
-        return habitacionesContext;
-    case '/galeria.html':
-        return {};
-    case '/reservaciones.html':
-        return reservacionesContext;
-    case '/promociones.html':
-        return {};
-    case '/testimonios.html':
-        return testimoniosContext;
-        */
+        case '/testimonios.html':
+            return testimoniosContext;
+        case '/promociones.html':
+            return promocionesContext;
+        case '/reservaciones.html':
+            return reservacionesContext;
+        case '/galeria.html':
+            return {};
         default:
             return {};
     }
